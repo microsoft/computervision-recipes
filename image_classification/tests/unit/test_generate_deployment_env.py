fbea2b681227740e531f0b07226a01bc5c4fd8a8
Alexandra Teste <alteste@microsoft.com>
2019-03-23 23:04:00 -0700

    With the changes described before ...

---
+++ image_classification/tests/unit/test_generate_deployment_env.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append("../../../")
+
+from utils_cv.generate_deployment_env import generate_yaml
+
+
+def test_generate_yaml():
+    """Tests creation of deployment-specific yaml file
+    from existing image_classification/environment.yml"""
+    generate_yaml(directory='../../', ref_filename='environment.yml',
+                  needed_libraries=['fastai', 'pytorch'],
+                  conda_filename='mytestyml.yml')
+
+    assert os.path.exists(os.path.join(os.getcwd(), 'mytestyml.yml'))
+    os.remove(os.path.join(os.getcwd(), 'mytestyml.yml'))
