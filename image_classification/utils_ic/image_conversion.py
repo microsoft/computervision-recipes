3a11f2a78a015136f097fd4bd43e8ff32b8f84bd
Alexandra Teste <alteste@microsoft.com>
2019-03-23 22:49:11 -0700

    Made all discussed changes: no training, workspace simplification, formatting, introduction, updated text, shorter ipynb name, image conversion to base64 encoded, deployment-specific yaml file script, runner vs run, unneeded libraries, unneeded files, local setup, google docstrings, license, table of content

--- image_classification/utils_ic/image_conversion.py
+++ image_classification/utils_ic/image_conversion.py
@@ -1,52 +1,45 @@
 # python regular libraries
-from base64 import b64encode, b64decode
 import json
 import os
-from typing import Tuple
 
-# fast.ai
-from fastai.vision import open_image
+from base64 import b64encode
 
 
-def image2bytes(im_list: list, im_dir: str) -> bytes:
+def im2base64(im_name: str, im_dir: str) -> bytes:
     """
 
-    :param im_list: (list of strings) List of image file names
-    :param im_dir: (string) Directory name
-    :return: List containing the byte arrays of each input image
+    Args:
+        im_name (string): Image file name
+        im_dir (string): Image directory name
+
+    Returns: im_bytes
+
     """
 
-    string_content_list = []
-    for im_name in im_list:
-        with open(os.path.join(im_dir, im_name), "rb") as image:
-            # Extract image bytes
-            im_content = image.read()
-            # Convert bytes into a string
-            str_im_content = im_content.decode('ISO-8859-1')
-            # Append to list of strings
-            string_content_list.append(str_im_content)
+    with open(os.path.join(im_dir, im_name), "rb") as image:
+        # Extract image bytes
+        im_content = image.read()
+        # Convert bytes into a string
+        im_bytes = b64encode(im_content)
 
-    concatenated_list = '|||||'.join(string_content_list)
-    bytes_of_images = bytes(concatenated_list.encode('ISO-8859-1'))
-
-    return bytes_of_images
+    return im_bytes
 
 
-def image2json(im_list: list, im_dir: str) -> json:
+def ims2json(im_list: list, im_dir: str) -> json:
     """
 
-    :param im_list: (list of strings) List of image file names
-    :param im_dir: (string) Directory name
-    :return: List containing the byte arrays of each input image
+    Args:
+        im_list (list of strings): List of image file names
+        im_dir (string): Directory name
+
+    Returns: input_to_service: String containing the based64-encoded images
+    decoded into strings
+
     """
 
     im_string_list = []
     for im_name in im_list:
-        with open(os.path.join(im_dir, im_name), "rb") as image:
-            # Extract image bytes
-            im_content = image.read()
-            # Convert bytes into a string
-            im_string_list.append(b64encode(im_content).decode('utf-8'))
+        im_string_list.append(im2base64(im_name, im_dir).decode('utf-8'))
 
     input_to_service = json.dumps({'data': im_string_list})
 
