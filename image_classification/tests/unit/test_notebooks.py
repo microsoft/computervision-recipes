b612fdf8906905a9ed934db4b3b29691bbe4f1ac
Alexandra Teste <alteste@microsoft.com>
2019-04-04 14:07:47 -0700

    Uncommented datasets and experiments unittests + commented out the deployment notebook one

--- image_classification/tests/unit/test_notebooks.py
+++ image_classification/tests/unit/test_notebooks.py
@@ -2,10 +2,11 @@
 # https://github.com/Microsoft/Recommenders/tree/master/tests
 
 
-import glob
-import os
+# import glob
+# import os
 import papermill as pm
-import shutil
+
+# import shutil
 from utils_ic.datasets import Urls, unzip_url
 
 # Unless manually modified, python3 should be
@@ -38,39 +39,39 @@ def test_01_notebook_run(notebooks):
     )
 
 
-def test_deploy_1_notebook_run(notebooks):
-    notebook_path = notebooks["deploy_on_ACI"]
-    pm.execute_notebook(
-        notebook_path,
-        OUTPUT_NOTEBOOK,
-        parameters=dict(
-            PM_VERSION=pm.__version__,
-            DATA_PATH=unzip_url(Urls.fridge_objects_path, exist_ok=True),
-        ),
-        kernel_name=KERNEL_NAME,
-    )
-    try:
-        os.remove("myenv.yml")
-    except OSError:
-        pass
-    try:
-        os.remove("score.py")
-    except OSError:
-        pass
-
-    try:
-        os.remove("output.ipynb")
-    except OSError:
-        pass
-
-    # There should be only one file, but the name may be changed
-    file_list = glob.glob("./*.pkl")
-    for filePath in file_list:
-        try:
-            os.remove(filePath)
-        except OSError:
-            pass
-
-    shutil.rmtree(os.path.join(os.getcwd(), "azureml-models"))
-    shutil.rmtree(os.path.join(os.getcwd(), "models"))
-    shutil.rmtree(os.path.join(os.getcwd(), "outputs"))
+# def test_deploy_1_notebook_run(notebooks):
+#     notebook_path = notebooks["deploy_on_ACI"]
+#     pm.execute_notebook(
+#         notebook_path,
+#         OUTPUT_NOTEBOOK,
+#         parameters=dict(
+#             PM_VERSION=pm.__version__,
+#             DATA_PATH=unzip_url(Urls.fridge_objects_path, exist_ok=True),
+#         ),
+#         kernel_name=KERNEL_NAME,
+#     )
+#     try:
+#         os.remove("myenv.yml")
+#     except OSError:
+#         pass
+#     try:
+#         os.remove("score.py")
+#     except OSError:
+#         pass
+#
+#     try:
+#         os.remove("output.ipynb")
+#     except OSError:
+#         pass
+#
+#     # There should be only one file, but the name may be changed
+#     file_list = glob.glob("./*.pkl")
+#     for filePath in file_list:
+#         try:
+#             os.remove(filePath)
+#         except OSError:
+#             pass
+#
+#     shutil.rmtree(os.path.join(os.getcwd(), "azureml-models"))
+#     shutil.rmtree(os.path.join(os.getcwd(), "models"))
+#     shutil.rmtree(os.path.join(os.getcwd(), "outputs"))
