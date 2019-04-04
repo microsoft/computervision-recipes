a293079760a179f857ec6540c160e47f7d3bfe57
Alexandra Teste <alteste@microsoft.com>
2019-04-04 10:24:43 -0700

    Changed way to get to the test images + updated image file names

--- image_classification/tests/unit/test_image_conversion.py
+++ image_classification/tests/unit/test_image_conversion.py
@@ -3,16 +3,18 @@
 
 import os
 
+from constants import TEMP_DIR
 from utils_ic.datasets import Urls, unzip_url
 from utils_ic.image_conversion import im2base64, ims2strlist
 
 
 def test_ims2strlist():
     """ Tests extraction of image content and conversion into string"""
-    data_path = unzip_url(Urls.fridge_objects_path, exist_ok=True)
+    os.makedirs(TEMP_DIR, exist_ok=True)
+    data_path = unzip_url(Urls.fridge_objects_path, TEMP_DIR, exist_ok=True)
     im_list = [
-        os.path.join(data_path, "can", "im_1.jpg"),
-        os.path.join(data_path, "carton", "im_62.jpg"),
+        os.path.join(data_path, "can", "1.jpg"),
+        os.path.join(data_path, "carton", "62.jpg"),
     ]
     im_string_list = ims2strlist(im_list)
     assert isinstance(im_string_list, list)
@@ -20,7 +22,8 @@ def test_ims2strlist():
 
 def test_im2base64():
     """ Tests extraction of image content and conversion into bytes"""
-    data_path = unzip_url(Urls.fridge_objects_path, exist_ok=True)
-    im_name = os.path.join(data_path, "can", "im_1.jpg")
+    os.makedirs(TEMP_DIR, exist_ok=True)
+    data_path = unzip_url(Urls.fridge_objects_path, TEMP_DIR, exist_ok=True)
+    im_name = os.path.join(data_path, "can", "1.jpg")
     im_content = im2base64(im_name)
     assert isinstance(im_content, bytes)
