aecc19f1e2c26ae7042b3794076013e87ebf8c87
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2019-04-05 12:52:33 -0400

    Fastai train-metrics callback class (#87)
    * Fastai train-metrics callback class
    
    Add custom callback class
    - Record metrics on the training set
    - Draw result table and graphs while training
    
    Update 01_notebook to use that
    
    Add unittest

--- image_classification/tests/conftest.py
+++ image_classification/tests/conftest.py
@@ -8,7 +8,13 @@
 # automatically gets discovered by pytest."
 
 import os
+from pathlib import Path
 import pytest
+from tempfile import TemporaryDirectory
+
+from fastai.vision import ImageList, imagenet_stats
+
+from utils_ic.datasets import unzip_url, Urls
 
 
 def path_notebooks():
@@ -28,8 +34,44 @@ def notebooks():
         "01_training_introduction": os.path.join(
             folder_notebooks, "01_training_introduction.ipynb"
         ),
-        "deploy_on_ACI": os.path.join(folder_notebooks,
-                                      "deployment", "01_deployment_on_"
-                                      "azure_container_instances.ipynb"),
+        "deploy_on_ACI": os.path.join(
+            folder_notebooks,
+            "deployment",
+            "01_deployment_on_" "azure_container_instances.ipynb",
+        ),
     }
     return paths
+
+
+@pytest.fixture
+def tmp(tmp_path_factory):
+    """Create a function-scoped temp directory.
+    Will be cleaned up after each test function.
+
+    Args:
+        tmp_path_factory (pytest.TempPathFactory): Pytest default fixture
+
+    Returns:
+        str: Temporary directory path
+    """
+    with TemporaryDirectory(dir=tmp_path_factory.getbasetemp()) as td:
+        yield td
+
+
+@pytest.fixture
+def tiny_ic_data(tmp):
+    """Load tiny image-classification data for a test.
+    TODO refactor to use actual `tiny` data once we have it.
+
+    Returns:
+        ImageDataBunch
+    """
+    path = Path(unzip_url(Urls.fridge_objects_path, tmp, exist_ok=True))
+    return (
+        ImageList.from_folder(path)
+        .split_by_rand_pct(valid_pct=0.2, seed=10)
+        .label_from_folder()
+        .transform(size=299)
+        .databunch(bs=16)
+        .normalize(imagenet_stats)
+    )
