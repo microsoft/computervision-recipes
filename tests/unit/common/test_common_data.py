446dd77e61b6f56d30f6e4334842db5937eb4da5
Jun Ki Min <42475935+loomlike@users.noreply.github.com>
2019-04-25 15:35:25 -0400

    Refactor (#127)
    * Initial refactor
    
    - All python scripts under utils_cv are clean
    - All files under classification (notebooks and tools-scripts)
      and tests need fix
    
    * refactor gpu util
    
    * datapath
    
    * test refactor
    
    * widgets
    
    * fix tests. fix 00 10 21 notebooks
    
    * Update 22 notebook. Add nbconverted files
    
    * python version
    
    * Root readme.md
    
    * faq
    
    * change test filenames otherwise they fail
    
    * conftest update
    
    * Fix result widget import
    
    * Fix 01 notebook widget import
    
    * Refactor widget.py and misc.py
    
    * azure pipelines update
    
    * azure pipelines update
    
    * update tests to skip deployment notebook
    
    * Removed links to services + added a small comment on existing AKS compute resources

---
+++ tests/unit/common/test_common_data.py
@@ -0,0 +1,82 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+import pytest
+import shutil
+from pathlib import Path
+from typing import Union
+from utils_cv.classification.data import Urls
+from utils_cv.common.data import (
+    data_path,
+    get_files_in_directory,
+    unzip_url,
+    root_path,
+)
+
+
+def test_root_path():
+    s = root_path()
+    assert isinstance(s, Path) and s != ""
+
+
+def test_data_path():
+    s = data_path()
+    assert isinstance(s, Path) and s != ""
+
+
+def test_get_files_in_directory(tiny_ic_data_path):
+    im_dir = os.path.join(tiny_ic_data_path, "can")
+    assert len(get_files_in_directory(im_dir)) == 22
+    assert len(get_files_in_directory(im_dir, suffixes=[".jpg"])) == 22
+    assert len(get_files_in_directory(im_dir, suffixes=[".nonsense"])) == 0
+
+
+def _test_url_data(url: str, path: Union[Path, str], dir_name: str):
+    data_path = unzip_url(url, fpath=path, dest=path, exist_ok=True)
+    # assert zip file exists
+    assert os.path.exists(os.path.join(path, f"{dir_name}.zip"))
+    # assert unzipped file (titled {dir_name}) exists
+    assert os.path.exists(os.path.join(path, dir_name))
+    # assert unzipped file equals the returned {data_path}
+    assert os.path.realpath(os.path.join(path, dir_name)) == os.path.realpath(
+        data_path
+    )
+
+
+def test_unzip_url_rel_path(tmp_path):
+    """ Test unzip with relative path. """
+    rel_path = tmp_path
+    _test_url_data(Urls.fridge_objects_path, rel_path, "fridgeObjects")
+
+
+def test_unzip_url_abs_path(tmp_path):
+    """ Test unzip with absolute path. """
+    abs_path = Path(os.path.abspath(tmp_path))
+    _test_url_data(Urls.fridge_objects_path, abs_path, "fridgeObjects")
+
+
+def test_unzip_url_exist_ok(tmp_path):
+    """
+    Test if exist_ok is true and (file exists, file does not exist)
+    """
+    os.makedirs(tmp_path / "fridgeObjects")
+    fridge_object_path = unzip_url(
+        Urls.fridge_objects_path, tmp_path, exist_ok=True
+    )
+    assert len(os.listdir(fridge_object_path)) == 0
+    shutil.rmtree(tmp_path / "fridgeObjects")
+    fridge_object_path = unzip_url(Urls.recycle_path, tmp_path, exist_ok=True)
+    assert len(os.listdir(fridge_object_path)) > 0
+
+
+def test_unzip_url_not_exist_ok(tmp_path):
+    """
+    Test if exist_ok is false and (file exists, file does not exist)
+    """
+    os.makedirs(tmp_path / "fridgeObjects")
+    with pytest.raises(FileExistsError):
+        unzip_url(Urls.fridge_objects_path, tmp_path, exist_ok=False)
+    shutil.rmtree(tmp_path / "fridgeObjects")
+    os.remove(tmp_path / "fridgeObjects.zip")
+    unzip_url(Urls.fridge_objects_path, tmp_path, exist_ok=False)
---
+++ utils_cv/common/data.py
@@ -0,0 +1,128 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+from pathlib import Path
+import requests
+from typing import List, Union
+from urllib.parse import urlparse
+from zipfile import ZipFile
+
+
+def data_path() -> Path:
+    """Get the data directory path"""
+    data_dir = Path(
+        os.path.realpath(
+            os.path.join(
+                os.path.dirname(__file__), os.pardir, os.pardir, "data"
+            )
+        )
+    )
+    data_dir.mkdir(exist_ok=True)
+    return data_dir
+
+
+def get_files_in_directory(
+    directory: str, suffixes: List[str] = None
+) -> List[str]:
+    """Returns all filenames in a directory which optionally match one of multiple suffixes.
+    Args:
+        directory: directory to scan for files.
+        suffixes: only keep the filenames which ends with one of the suffixes
+            (e.g. suffixes = [".jpg", ".png", ".gif"]).
+    Return:
+        List of filenames
+    """
+    if not os.path.exists(directory):
+        raise Exception(f"Directory '{directory}' does not exist.")
+    filenames = [str(p) for p in Path(directory).iterdir() if p.is_file()]
+    if suffixes and suffixes != "":
+        filenames = [
+            s for s in filenames if s.lower().endswith(tuple(suffixes))
+        ]
+    return filenames
+
+
+def _get_file_name(url: str) -> str:
+    """ Get a file name based on url. """
+    return urlparse(url).path.split("/")[-1]
+
+
+def unzip_url(
+    url: str,
+    fpath: Union[Path, str] = data_path(),
+    dest: Union[Path, str] = data_path(),
+    exist_ok: bool = False,
+) -> str:
+    """ Download file from URL to {fpath} and unzip to {dest}.
+    {fpath} and {dest} must be directories
+
+    Args:
+        url (str): url to download from
+        fpath (Union[Path, str]): The location to save the url zip file to
+        dest (Union[Path, str]): The destination to unzip {fpath}
+        exist_ok (bool): if exist_ok, then skip if exists, otherwise throw error
+
+    Raises:
+        FileExistsError: if file exists
+
+    Returns:
+        Path of {dest}
+    """
+
+    def _raise_file_exists_error(path: Union[Path, str]) -> None:
+        if not exist_ok:
+            raise FileExistsError(path, "Use param {{exist_ok}} to ignore.")
+
+    os.makedirs(dest, exist_ok=True)
+    os.makedirs(fpath, exist_ok=True)
+
+    fname = _get_file_name(url)
+    fname_without_extension = fname.split(".")[0]
+    zip_file = Path(os.path.join(fpath, fname))
+    unzipped_dir = Path(os.path.join(fpath, fname_without_extension))
+
+    # download zipfile if zipfile not exists
+    if zip_file.is_file():
+        _raise_file_exists_error(zip_file)
+    else:
+        r = requests.get(url)
+        f = open(zip_file, "wb")
+        f.write(r.content)
+        f.close()
+
+    # unzip downloaded zipfile if dir not exists
+    if unzipped_dir.is_dir():
+        _raise_file_exists_error(unzipped_dir)
+    else:
+        z = ZipFile(zip_file, "r")
+        z.extractall(fpath)
+        z.close()
+
+    return os.path.realpath(os.path.join(fpath, fname_without_extension))
+
+
+def unzip_urls(
+    urls: List[str], dest: Union[Path, str] = data_path()
+) -> List[str]:
+    """ Download and unzip all datasets in Urls to dest """
+
+    # make dir if not exist
+    if not Path(dest).is_dir():
+        os.makedirs(dest)
+
+    # download all data urls
+    paths = list()
+    for url in urls:
+        paths.append(unzip_url(url, dest, exist_ok=True))
+
+    return paths
+
+
+def root_path() -> Path:
+    """Get path of root dir."""
+    return Path(
+        os.path.realpath(
+            os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+        )
+    )
