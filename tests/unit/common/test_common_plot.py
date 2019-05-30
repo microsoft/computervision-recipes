bf2722f902e4e91e020291d13ed72a958abd91e5
PatrickBue <pabuehle@microsoft.com>
2019-05-30 10:11:10 -0400

    added missing lincense headers

--- tests/unit/common/test_common_plot.py
+++ tests/unit/common/test_common_plot.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 from pathlib import Path
 import matplotlib.pyplot as plt
 from utils_cv.common.plot import line_graph, show_ims
