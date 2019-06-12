4e95c6809e830f59e9426b1d7c1ba8192b565f7c
ateste <47578822+ateste@users.noreply.github.com>
2019-06-12 13:49:39 -0700

    Image Similarity 00 and 01 notebooks [Unittests] (#202)
    Merging image similarity into staging

---
+++ tests/unit/similarity/test_similarity_metrics.py
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from utils_cv.similarity.metrics import recall_at_k  # positive_image_rank_list
+
+
+def test_positive_image_rank_list(testing_im_list):
+    pass
+    # similarity_tuple = [
+    #    (im_path, testing_im_list.index(im_path) * 1.0)
+    #    for im_path in testing_im_list
+    # ]
+    # similarity_tuple_list = [similarity_tuple] * 3
+    # assert positive_image_rank_list(similarity_tuple_list) == [1, 1, 1]
+
+
+def test_recall_at_k():
+    rank_list = list(range(10))
+    threshold = 5
+    assert recall_at_k(rank_list, threshold) == 60.0
