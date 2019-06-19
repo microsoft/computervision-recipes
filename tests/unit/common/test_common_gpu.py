38821f4437b3c9e299c2ddbf791ff6f4a9b2b156
PatrickBue <pabuehle@microsoft.com>
2019-06-19 20:42:22 +0000

    removed gpu_info function (#235)

--- tests/unit/common/test_common_gpu.py
+++ tests/unit/common/test_common_gpu.py
@@ -1,20 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import torch.cuda as cuda
-from utils_cv.common.gpu import gpu_info, which_processor
-
-
-def test_gpu_info():
-    gpus = gpu_info()
-    # Check if torch.cuda returns the same number of gpus
-    assert cuda.device_count() == len(gpus)
-
-    for i in range(len(gpus)):
-        # Check if torch.cuda returns the same device name
-        assert gpus[i]["device_name"] == cuda.get_device_name(i)
-        # Total memory should be grater than used-memory
-        assert int(gpus[i]["total_memory"]) > int(gpus[i]["used_memory"])
+from utils_cv.common.gpu import which_processor
 
 
 def test_which_processor():
