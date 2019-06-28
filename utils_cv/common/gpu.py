62924cf68249e24c63f6223dca69cfa71150cb90
Ubuntu <pabuehle@patrick.q2ztiabuqklepgi1mxb0jepdzb.bx.internal.cloudapp.net>
2019-06-28 15:30:06 +0000

    updated test

--- utils_cv/common/gpu.py
+++ utils_cv/common/gpu.py
@@ -11,3 +11,12 @@ def which_processor():
         print(f"Fast.ai (Torch) is using GPU: {get_device_name(device_nr)}")
     else:
         print("Cuda is not available. Fast.ai/Torch is using CPU")
+
+
+# Helper function to check if GPU machine which linux.
+# Integration tests are too slow on Windows/CPU machines.
+def linux_with_gpu():
+    """Returns if machine is running an Linux OS and has a GPU"""
+    is_linux = platform.system().lower() == "linux"
+    has_gpu = is_available()
+    return is_linux and has_gpu
