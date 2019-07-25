f3f652944f72397db26c2ccdea140cab8ad93a09
PatrickBue <pabuehle@microsoft.com>
2019-07-25 13:03:32 -0400

    replaced if-linux-gpu with marker

--- tests/integration/similarity/test_integration_similarity_notebooks.py
+++ tests/integration/similarity/test_integration_similarity_notebooks.py
@@ -4,9 +4,6 @@
 import papermill as pm
 import pytest
 import scrapbook as sb
-from torch.cuda import is_available
-
-from utils_cv.common.gpu import linux_with_gpu
 
 # Parameters
 KERNEL_NAME = "python3"
@@ -14,15 +11,15 @@ OUTPUT_NOTEBOOK = "output.ipynb"
 
 
 @pytest.mark.notebooks
+@pytest.mark.linuxgpu
 def test_01_notebook_run(similarity_notebooks):
-    if linux_with_gpu():
-        notebook_path = similarity_notebooks["01"]
-        pm.execute_notebook(
-            notebook_path,
-            OUTPUT_NOTEBOOK,
-            parameters=dict(PM_VERSION=pm.__version__),
-            kernel_name=KERNEL_NAME,
-        )
+    notebook_path = similarity_notebooks["01"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        parameters=dict(PM_VERSION=pm.__version__),
+        kernel_name=KERNEL_NAME,
+    )
 
-        nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
-        assert nb_output.scraps["median_rank"].data <= 10
+    nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
+    assert nb_output.scraps["median_rank"].data <= 10
