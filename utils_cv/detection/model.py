2394e970e1295c39d2e6b60d7ebd3667f86e9fd3
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2019-08-29 09:57:43 +0800

    Add object detection 00 webcam notebook (#297)

--- utils_cv/detection/model.py
+++ utils_cv/detection/model.py
@@ -34,9 +34,9 @@ def _get_det_bboxes(
     Return:
         a list of DetectionBboxes
     """
-    pred_labels = list(pred[0]["labels"].cpu().numpy())
-    pred_boxes = list(pred[0]["boxes"].detach().cpu().numpy().astype(np.int32))
-    pred_scores = list(pred[0]["scores"].detach().cpu().numpy())
+    pred_labels = pred[0]["labels"].detach().cpu().numpy().tolist()
+    pred_boxes = pred[0]["boxes"].detach().cpu().numpy().astype(np.int32).tolist()
+    pred_scores = pred[0]["scores"].detach().cpu().numpy().tolist()
 
     det_bboxes = []
     for label, box, score in zip(pred_labels, pred_boxes, pred_scores):
