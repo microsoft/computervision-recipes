19c4adcd1fcca066404cb71066211e3ab258f207
PatrickBue <patrick.buehler@gmail.com>
2019-09-10 22:00:55 +0000

    changed so that label_idx 0 is never used

--- utils_cv/detection/dataset.py
+++ utils_cv/detection/dataset.py
@@ -162,6 +162,7 @@ class DetectionDataset:
             collate_fn=collate_fn,
         )
 
+
     def _read_annos(self) -> List[str]:
         """ Parses all Pascal VOC formatted annotation files to extract all
         possible labels. """
@@ -203,7 +204,7 @@ class DetectionDataset:
             self.anno_bboxes.append(anno_bboxes)
 
         # Get list of all labels
-        labels = ["__background__"]
+        labels = [] #["__background__"]
         for anno_bboxes in self.anno_bboxes:
             for anno_bbox in anno_bboxes:
                 labels.append(anno_bbox.label_name)
@@ -212,7 +213,8 @@ class DetectionDataset:
         # Set for each bounding box label name also what its integer representation is
         for anno_bboxes in self.anno_bboxes:
             for anno_bbox in anno_bboxes:
-                anno_bbox.label_idx = self.labels.index(anno_bbox.label_name)
+                anno_bbox.label_idx = self.labels.index(anno_bbox.label_name) +1
+                
 
     def split_train_test(
         self, train_pct: float = 0.8
