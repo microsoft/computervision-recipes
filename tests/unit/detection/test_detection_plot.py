17c2e27dae2ca9cead0eaaec4b60d42331e0a8d5
jiata <jiata@microsoft.com>
2019-09-23 20:50:20 +0000

    linux gpu test

--- tests/unit/detection/test_detection_plot.py
+++ tests/unit/detection/test_detection_plot.py
@@ -98,11 +98,13 @@ def test__get_precision_recall_settings():
     assert len(ret) == 5
 
 
+@pytest.mark.linuxgpu
 def test__plot_pr_curve_iou_range(od_detection_eval, basic_ax):
     """ Test that `_plot_pr_curve_iou_range` works. """
     _plot_pr_curve_iou_range(basic_ax, od_detection_eval.coco_eval["bbox"])
 
 
+@pytest.mark.linuxgpu
 def test__plot_pr_curve_iou_mean(od_detection_eval, basic_ax):
     """ Test that `_plot_pr_curve_iou_mean` works. """
     _plot_pr_curve_iou_mean(basic_ax, od_detection_eval.coco_eval["bbox"])
