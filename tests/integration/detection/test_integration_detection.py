380213edc10de3bb3c3ab08d4d0ea4e23daf69b8
jiata <jiata@microsoft.com>
2019-09-23 16:33:00 +0000

    detection integration bug fix

--- tests/integration/detection/test_integration_detection.py
+++ tests/integration/detection/test_integration_detection.py
@@ -13,18 +13,16 @@ OUTPUT_NOTEBOOK = "output.ipynb"
 @pytest.mark.notebooks
 @pytest.mark.linuxgpu
 def test_01_notebook_run(detection_notebooks):
+    epochs = 3
     notebook_path = detection_notebooks["01"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
-        parameters=dict(
-            PM_VERSION=pm.__version__,
-            EPOCHS=5,
-        ),
+        parameters=dict(PM_VERSION=pm.__version__, EPOCHS=epochs),
         kernel_name=KERNEL_NAME,
     )
 
     nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
-    assert len(nb_output.scraps["training_losses"].data) == 10
-    assert nb_output.scraps["training_losses"].data[-1] > 0.5
-    assert nb_output.scraps["training_average_precision"].data > 0.8
+    assert len(nb_output.scraps["training_losses"].data) == epochs
+    assert nb_output.scraps["training_losses"].data[-1] < 0.5
+    assert nb_output.scraps["training_average_precision"].data[-1] > 0.5
