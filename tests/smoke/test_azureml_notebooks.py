083e527bb1892a2afaace168304355e6b96806cf
PatrickBue <pabuehle@microsoft.com>
2019-10-03 23:47:54 +0000

    Hyperdrive notebook updates (#356)
    All tests are passing (except for unrelated AML deployment notebooks)

--- tests/smoke/test_azureml_notebooks.py
+++ tests/smoke/test_azureml_notebooks.py
@@ -3,6 +3,7 @@
 
 import papermill as pm
 import pytest
+import scrapbook as sb
 
 # Unless manually modified, cv should be
 # the name of the current jupyter kernel
@@ -11,8 +12,11 @@ KERNEL_NAME = "cv"
 OUTPUT_NOTEBOOK = "output.ipynb"
 
 
+# ----- Image classification ----------------------------------------------------------
+
+
 @pytest.mark.azuremlnotebooks
-def test_20_notebook_run(
+def test_ic_20_notebook_run(
     classification_notebooks,
     subscription_id,
     resource_group,
@@ -35,7 +39,7 @@ def test_20_notebook_run(
 
 
 @pytest.mark.azuremlnotebooks
-def test_21_notebook_run(
+def test_ic_21_notebook_run(
     classification_notebooks,
     subscription_id,
     resource_group,
@@ -60,7 +64,7 @@ def test_21_notebook_run(
 
 
 @pytest.mark.azuremlnotebooks
-def test_22_notebook_run(
+def test_ic_22_notebook_run(
     classification_notebooks,
     subscription_id,
     resource_group,
@@ -85,7 +89,7 @@ def test_22_notebook_run(
 
 
 @pytest.mark.azuremlnotebooks
-def test_23_notebook_run(
+def test_ic_23_notebook_run(
     classification_notebooks,
     subscription_id,
     resource_group,
@@ -108,7 +112,7 @@ def test_23_notebook_run(
 
 
 @pytest.mark.azuremlnotebooks
-def test_24_notebook_run(
+def test_ic_24_notebook_run(
     classification_notebooks,
     subscription_id,
     resource_group,
@@ -135,4 +139,34 @@ def test_24_notebook_run(
     )
 
 
-# TODO add test for hyperparam object detection notebook
+# # ----- Object detection ----------------------------------------------------------
+
+
+@pytest.mark.azuremlnotebooks
+def test_od_11_notebook_run(
+    detection_notebooks,
+    subscription_id,
+    resource_group,
+    workspace_name,
+    workspace_region,
+):
+    notebook_path = detection_notebooks["11"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        parameters=dict(
+            PM_VERSION=pm.__version__,
+            subscription_id=subscription_id,
+            resource_group=resource_group,
+            workspace_name=workspace_name,
+            workspace_region=workspace_region,
+            MAX_NODES=3,
+            IM_MAX_SIZES=[200],
+            LEARNING_RATES=[1e-5, 3e-3],
+            UTILS_DIR="utils_cv",
+        ),
+        kernel_name=KERNEL_NAME,
+    )
+
+    nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
+    assert nb_output.scraps["best_accuracy"].data > 0.70
