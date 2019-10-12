d085295d32039916c3f595671baeda4fc29c05ba
PatrickBue <pabuehle@microsoft.com>
2019-10-12 19:42:51 +0000

    added notebook tests

--- tests/integration/detection/test_integration_detection.py
+++ tests/integration/detection/test_integration_detection.py
@@ -26,3 +26,21 @@ def test_01_notebook_run(detection_notebooks):
     assert len(nb_output.scraps["training_losses"].data) == epochs
     assert nb_output.scraps["training_losses"].data[-1] < 0.5
     assert nb_output.scraps["training_average_precision"].data[-1] > 0.5
+
+@pytest.mark.notebooks
+@pytest.mark.linuxgpu
+def test_12_notebook_run(detection_notebooks):
+    epochs = 3
+    notebook_path = detection_notebooks["12"]
+    pm.execute_notebook(
+        notebook_path,
+        OUTPUT_NOTEBOOK,
+        parameters=dict(PM_VERSION=pm.__version__, EPOCHS=epochs),
+        kernel_name=KERNEL_NAME,
+    )
+
+    nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
+    assert nb_output.scraps["valid_accs"].data[-1] > 0.5
+    assert len(nb_output.scraps["valid_accs"].data) == epochs
+    assert len(nb_output.scraps["hard_im_scores"].data) == 10
+
