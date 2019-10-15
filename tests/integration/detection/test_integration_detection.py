15cc36ff440f480e9d4c45528c07d2efa67b6040
PatrickBue <pabuehle@microsoft.com>
2019-10-15 19:04:58 +0000

    fixed integration test

--- tests/integration/detection/test_integration_detection.py
+++ tests/integration/detection/test_integration_detection.py
@@ -30,17 +30,16 @@ def test_01_notebook_run(detection_notebooks):
 @pytest.mark.notebooks
 @pytest.mark.linuxgpu
 def test_12_notebook_run(detection_notebooks):
-    epochs = 3
     notebook_path = detection_notebooks["12"]
     pm.execute_notebook(
         notebook_path,
         OUTPUT_NOTEBOOK,
-        parameters=dict(PM_VERSION=pm.__version__, EPOCHS=epochs),
+        parameters=dict(PM_VERSION=pm.__version__, EPOCHS=3),
         kernel_name=KERNEL_NAME,
     )
 
     nb_output = sb.read_notebook(OUTPUT_NOTEBOOK)
     assert nb_output.scraps["valid_accs"].data[-1] > 0.5
-    assert len(nb_output.scraps["valid_accs"].data) == epochs
+    assert len(nb_output.scraps["valid_accs"].data) == 1
     assert len(nb_output.scraps["hard_im_scores"].data) == 10
 
