d1fcf307ea3d73215bee0a819e25ea7d8127107b
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2019-12-06 14:32:50 +0800

    Add pretrained keypoint model (#453)
    * Add pretrained keypoint model
    
    * Fix bugs in tests
    
    * Add 03 notebook in conftest.py
    
    * Minor revision
    
    * Reformat code using black

--- tests/unit/detection/test_detection_mask.py
+++ tests/unit/detection/test_detection_mask.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 import numpy as np
 from utils_cv.detection.mask import (
     binarise_mask,
@@ -19,8 +22,7 @@ def test_colorise_binary_mask(od_mask_rects):
     foreground = 9
     background = 0
     colored_mask = colorise_binary_mask(
-        binary_mask,
-        color=(foreground, foreground, foreground)
+        binary_mask, color=(foreground, foreground, foreground)
     )
     for ch in colored_mask.transpose((2, 0, 1)):
         assert np.all(ch[binary_mask] == foreground)
@@ -33,8 +35,7 @@ def test_transparentise_mask(od_mask_rects):
     foreground = 9
     background = 0
     colored_mask = colorise_binary_mask(
-        binary_mask,
-        color=(foreground, foreground, foreground)
+        binary_mask, color=(foreground, foreground, foreground)
     )
     transparent_mask = transparentise_mask(colored_mask, alpha=0.7)
     assert np.all(transparent_mask[binary_mask] != background)
