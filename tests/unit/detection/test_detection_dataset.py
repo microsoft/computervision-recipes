2a948cf477ec5c7c21f3115ad237a14521516391
Simon Zhao <43029286+simonzhaoms@users.noreply.github.com>
2019-12-17 13:28:10 +0800

    Add keypoint detection with tuned model (#454)
    * Add keypoint detetion with tuned model
    
    * Add tests
    
    * Minor revision
    
    * Update tests
    
    * Fix bugs in tests
    
    * Use GPU device if available
    
    * Update tests
    
    * Fix bug: 'not idx' will be 'True' if 'idx=0'
    
    * Fix bugs
    
    * Move toy keypoint meta into notebook
    
    * Fix bugs
    
    * Fix bugs
    
    * Fix bugs in notebook
    
    * Add descriptions for keypoint meta data
    
    * Raise exception when RandomHorizontalFlip is used without specifying hflip_inds

--- tests/unit/detection/test_detection_dataset.py
+++ tests/unit/detection/test_detection_dataset.py
@@ -68,15 +68,34 @@ def test_get_transform(basic_im):
     assert type(tfms_im) == Tensor
 
 
-def test_parse_pascal_voc(od_sample_im_anno, od_sample_bboxes):
+def test_parse_pascal_voc(
+    od_sample_im_anno, od_sample_bboxes, tiny_od_keypoint_data_path
+):
     """ test that 'parse_pascal_voc' can parse the 'od_sample_im_anno' correctly. """
     anno_path, im_path = od_sample_im_anno
-    anno_bboxes, im_path = parse_pascal_voc_anno(anno_path)
+    anno_bboxes, im_path, _ = parse_pascal_voc_anno(anno_path)
     assert type(anno_bboxes[0]) == AnnotationBbox
     assert anno_bboxes[0].left == od_sample_bboxes[0].left
     assert anno_bboxes[0].right == od_sample_bboxes[0].right
     assert anno_bboxes[0].top == od_sample_bboxes[0].top
     assert anno_bboxes[0].bottom == od_sample_bboxes[0].bottom
+
+    # test keypoints
+    anno_path = Path(tiny_od_keypoint_data_path) / "annotations" / "9.xml"
+    keypoints_truth = np.array(
+        [
+            [
+                [328, 227, 2],
+                [382, 228, 2],
+                [326, 247, 2],
+                [382, 249, 2],
+                [302, 440, 2],
+                [379, 446, 2],
+            ]
+        ]
+    )
+    _, _, keypoints_pred = parse_pascal_voc_anno(anno_path)
+    np.all(keypoints_pred == keypoints_truth)
 
 
 def validate_detection_dataset(data: DetectionDataset, labels: List[str]):
@@ -90,10 +109,18 @@ def validate_detection_dataset(data: DetectionDataset, labels: List[str]):
         assert len(data.mask_paths) == len(data.im_paths)
 
 
+def validate_milkbottle_keypoint_tiny_dataset(data: DetectionDataset):
+    assert len(data) == 31
+    assert type(data) == DetectionDataset
+    assert len(data.labels) == 1
+    assert len(data.keypoints) == len(data.im_paths)
+
+
 def test_detection_dataset_init_basic(
     tiny_od_data_path,
     od_data_path_labels,
-    tiny_od_mask_data_path
+    tiny_od_mask_data_path,
+    tiny_od_keypoint_data_path,
 ):
     """ Tests that initialization of the Detection Dataset works. """
     data = DetectionDataset(tiny_od_data_path)
@@ -109,16 +136,46 @@ def test_detection_dataset_init_basic(
 
     # test mask data
     data = DetectionDataset(
-        tiny_od_mask_data_path,
-        mask_dir="segmentation-masks"
+        tiny_od_mask_data_path, mask_dir="segmentation-masks"
     )
     validate_detection_dataset(data, od_data_path_labels)
     assert len(data.test_ds) == 15
     assert len(data.train_ds) == 16
 
+    # test keypoint data
+    data = DetectionDataset(
+        tiny_od_keypoint_data_path,
+        keypoint_meta={
+            "labels": [
+                "lid_left_top",
+                "lid_right_top",
+                "lid_left_bottom",
+                "lid_right_bottom",
+                "left_bottom",
+                "right_bottom",
+            ],
+            "skeleton": [
+                [0, 1],
+                [0, 2],
+                [1, 3],
+                [2, 3],
+                [2, 4],
+                [3, 5],
+                [4, 5],
+            ],
+            "hflip_inds": [1, 0, 3, 2, 5, 4],
+        },
+    )
+    validate_milkbottle_keypoint_tiny_dataset(data)
+    assert len(data.test_ds) == 15
+    assert len(data.train_ds) == 16
+
 
 def test_detection_dataset_init_train_pct(
-    tiny_od_data_path, od_data_path_labels, tiny_od_mask_data_path
+    tiny_od_data_path,
+    od_data_path_labels,
+    tiny_od_mask_data_path,
+    tiny_od_keypoint_data_path,
 ):
     """ Tests that initialization with train_pct."""
     data = DetectionDataset(tiny_od_data_path, train_pct=0.75)
@@ -128,31 +185,62 @@ def test_detection_dataset_init_train_pct(
 
     # test mask data
     data = DetectionDataset(
-        tiny_od_mask_data_path,
-        train_pct=0.75,
-        mask_dir="segmentation-masks"
+        tiny_od_mask_data_path, train_pct=0.75, mask_dir="segmentation-masks"
     )
     validate_detection_dataset(data, od_data_path_labels)
+    assert len(data.test_ds) == 7
+    assert len(data.train_ds) == 24
+
+    # test keypoint data
+    data = DetectionDataset(
+        tiny_od_keypoint_data_path,
+        train_pct=0.75,
+        keypoint_meta={
+            "labels": [
+                "lid_left_top",
+                "lid_right_top",
+                "lid_left_bottom",
+                "lid_right_bottom",
+                "left_bottom",
+                "right_bottom",
+            ],
+            "skeleton": [
+                [0, 1],
+                [0, 2],
+                [1, 3],
+                [2, 3],
+                [2, 4],
+                [3, 5],
+                [4, 5],
+            ],
+            "hflip_inds": [1, 0, 3, 2, 5, 4],
+        },
+    )
+    validate_milkbottle_keypoint_tiny_dataset(data)
     assert len(data.test_ds) == 7
     assert len(data.train_ds) == 24
 
 
 def test_detection_dataset_show_ims(
     basic_detection_dataset,
-    od_detection_mask_dataset
+    od_detection_mask_dataset,
+    tiny_od_detection_keypoint_dataset,
 ):
     # simply test that this is error free for now
     basic_detection_dataset.show_ims()
     od_detection_mask_dataset.show_ims()
+    tiny_od_detection_keypoint_dataset.show_ims()
 
 
 def test_detection_dataset_show_im_transformations(
     basic_detection_dataset,
-    od_detection_mask_dataset
+    od_detection_mask_dataset,
+    tiny_od_detection_keypoint_dataset,
 ):
     # simply test that this is error free for now
     basic_detection_dataset.show_im_transformations()
     od_detection_mask_dataset.show_im_transformations()
+    tiny_od_detection_keypoint_dataset.show_im_transformations()
 
 
 def test_detection_dataset_init_anno_im_dirs(
