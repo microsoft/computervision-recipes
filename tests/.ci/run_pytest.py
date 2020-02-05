53603700fefdc1649d18969de33cdc351afc766d
PatrickBue <pabuehle@microsoft.com>
2020-02-05 15:03:20 -0500

    Fix bug, otherwise silently no tests were running

--- tests/.ci/run_pytest.py
+++ tests/.ci/run_pytest.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     logger.debug("junit= --junitxml={}".format(args.xmlname))
     pytest_cmd = [
         "pytest",
-        "--durations 100",
+        "--durations=100",
         "--ignore=contrib",
         args.testfolder,
         "-m",
