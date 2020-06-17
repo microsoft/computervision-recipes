7e97e1f6c4a5f442582aeb3170ab5b0682df024b
Casey Hong <jihon@microsoft.com>
2020-06-17 09:25:13 -0400

    rm merge conflict artifact and edit Urls class

--- utils_cv/tracking/data.py
+++ utils_cv/tracking/data.py
@@ -1,4 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from urllib.parse import urljoin
+
 class Urls:
-    fridge_objects_path = "https://cvbp.blob.core.windows.net/public/datasets/tracking/odFridgeObjects_FairMOT-Format.zip"
-    carcans_annotations_path = "https://cvbp.blob.core.windows.net/public/datasets/tracking/carcans_vott-csv-export.zip"
-    carcans_video_path = "https://cvbp.blob.core.windows.net/public/datasets/tracking/car_cans_8s.mp4"
+	base = (
+        "https://cvbp.blob.core.windows.net/public/datasets/tracking/"
+    )
+
+	fridge_objects_path = urljoin(base, "odFridgeObjects_FairMOT-Format.zip")
+	carcans_annotations_path = urljoin(base, "carcans_vott-csv-export.zip")
+	carcans_video_path = urljoin(base, "car_cans_8s.mp4")
+
+    @classmethod
+    def all(cls) -> List[str]:
+        return [v for k, v in cls.__dict__.items() if k.endswith("_path")]
